40cca49c5d66c3c7780f6c34da2a484e5bbc0910
rgup17 <rachitgupta428@gmail.com>
2024-02-11 13:05:45 -0500

    added memtest and mymalloc files

---
+++ memtest.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+
+// Compile with -DREALMALLOC to use the real malloc() instead of mymalloc()
+#ifndef REALMALLOC
+#include "mymalloc.h"
+#endif
+
+#define MEMSIZE 4096
+#define HEADERSIZE 8
+#define OBJECTS 64
+#define OBJSIZE (MEMSIZE / OBJECTS - HEADERSIZE)
+
+int main(int argc, char **argv)
+{
+	char *obj[OBJECTS];
+	int i, j, errors = 0;
+	
+	// fill memory with objects
+	for (i = 0; i < OBJECTS; i++) {
+		obj[i] = malloc(OBJSIZE);
+	}
+	
+	// fill each object with distinct bytes
+	for (i = 0; i < OBJECTS; i++) {
+		memset(obj[i], i, OBJSIZE);
+	}
+	
+	// check that all objects contain the correct bytes
+	for (i = 0; i < OBJECTS; i++) {
+		for (j = 0; j < OBJSIZE; j++) {
+			if (obj[i][j] != i) {
+				errors++;
+				printf("Object %d byte %d incorrect: %d\n", i, j, obj[i][j]);
+			}
+		}
+	}
+	
+	printf("%d incorrect bytes\n", errors);
+	
+	return EXIT_SUCCESS;
+}
