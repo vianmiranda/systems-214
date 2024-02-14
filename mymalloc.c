10a584293e5d9149bda1edfde6a055c2d5c38ea4
rgup17 <rachitgupta428@gmail.com>
2024-02-14 18:49:01 -0500

    added roundup and initializememory functions for mymalloc

--- mymalloc.c
+++ mymalloc.c
@@ -2,26 +2,25 @@
 #include <stdlib.h>
 #include "mymalloc.h" //reference the related header file
 
+/*
+  chunk:
+  1. header (metadata) - informs runtime system about chunk size and whether the chunk is allocated
+  2. payload (data) - the actual object/data, a contiguous sequence of bytes
+
+*/
+
+//makes sure size is rounded to nearest multiple of 8
+#define ROUNDUP8(x) (((x)+7) & (~7))
+
+// define metadata size (int for size and int for allocation status 1 or 0)
+#define METADATA_SIZE sizeof(int)*2
 
 //define memory block 
 static double memory[MEM_SIZE];
 
 
 
-/*
-size_t = size of requested memory block
-file = the source file
-line = line number of the application
-
-
-chunk:
-  1. header (metadata) - informs runtime system about chunk size and whether the chunk is allocated
-  2. payload (data) - the actual object/data, a contiguous sequence of bytes
-
-*/
-
 //create a struct for the chunk header. this will act as a LL to move from one chunk to next available chunk
-
 typedef struct chunkheader {
   size_t size;
   int is_allocated; //int can be represented as true/false
@@ -31,14 +30,39 @@ typedef struct chunkheader {
 //initially, list of free chunks is null
 chunkheader* free_chunk_list = NULL;
 
+
+//method to initialize memory and LL
+void initialize_memory() {
+  free_chunk_list = (chunkheader*)memory;
+  free_chunk_list->size = MEM_SIZE - METADATA_SIZE;
+  free_chunk_list->is_allocated = 0;
+  free_chunk_list->next = NULL;
+}
+
 /*
 instructions:
   1. search for free chunk of memory containing >= requested bytes to be allocated
   2. if chunk is large enough, you may divide the chunk into 2, 1st large enough for request
      (and return pointer to it) and 2nd remains free
-
 */
+
+
 void* mymalloc(size_t size, char *file, int line) {
+
+  //check if size == 0 and print error msg
+  if (size == 0) {
+    fprintf(stderr, "Can't allocate 0 bytes. File: %s, Line: %d\n", file, line);
+    return NULL;
+  }
+
+  //round to nearest multiple of 8
+  size = ROUNDUP8(size);
+
+  //initialize memory
+  if (free_chunk_list == NULL) {
+    initialize_memory();
+  }
+
   chunkheader* current = free_chunk_list;
   chunkheader* prev = NULL;
 
@@ -89,6 +113,7 @@ errors to detect:
 3. calling free a 2nd time on the same pointer
 */
 void myfree(void *ptr, char *file, int line) {
-
 }
+
+
 
