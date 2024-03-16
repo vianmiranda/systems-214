258305a07c44455c3f01044af3efe506d00e43c2
vianmiranda <38566642+vianmiranda@users.noreply.github.com>
2024-03-15 22:28:44 -0400

    Change trie to have 53 children (separate by capitalization)

--- spellcheck/triedict.c
+++ spellcheck/triedict.c
@@ -3,12 +3,14 @@
 #include <errno.h>
 #include <stdio.h>
 
+#define NUM_LETTERS 53
+
 typedef struct trie_node trie;
 
 struct trie_node {
-    char letter; // letter of the node (uppercase can only be uppercase, lowercase can be either)
+    char letter; // letter of the node in the trie
     int isWord; // 1 if word, 0 if not
-    trie* children[26]; 
+    trie* children[NUM_LETTERS]; 
 };
 
 trie* dict;
@@ -25,7 +27,7 @@ int init_trie() {
     }
     dict->letter = '\0';
     dict->isWord = 0;
-    for (int ii = 0; ii < 26; ii++) {
+    for (int ii = 0; ii < NUM_LETTERS; ii++) {
         dict->children[ii] = NULL;
     }
     return 0;
@@ -35,7 +37,17 @@ int add_word_to_trie(char* word) {
     trie* node = get_dict();
     for (int ii = 0; ii < strlen(word); ii++) {
         char letter = word[ii];
-        int index = letter - (letter >= 'A' && letter <= 'Z' ? 'A' : 'a');
+        int index;
+        if (letter == '\'') {
+            index = NUM_LETTERS - 1;
+        } else if (letter >= 'A' && letter <= 'Z') {
+            index = letter - 'A';
+        } else if (letter >= 'a' && letter <= 'z') {
+            index = letter - 'a' + 26;
+        } else {
+            fprintf(stderr, "Invalid character in word: %c\n", letter);
+            return -1;
+        }
         
         if (node->children[index] == NULL) {
             trie* new_node = malloc(sizeof(trie));
@@ -45,13 +57,14 @@ int add_word_to_trie(char* word) {
             }
             new_node->letter = letter;
             new_node->isWord = 0;
-            for (int jj = 0; jj < 26; jj++) {
+            for (int jj = 0; jj < NUM_LETTERS; jj++) {
                 new_node->children[jj] = NULL;
             }
             node->children[index] = new_node;
         }
         node = node->children[index];
     }
+    node->isWord = 1;
 
     return 0;
 }
