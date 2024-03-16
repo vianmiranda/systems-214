a3c5f5688b3dae60dbf758c0f534b9d3cb549829
vianmiranda <38566642+vianmiranda@users.noreply.github.com>
2024-03-15 22:31:05 -0400

    Clean text from leading/trailing punctuation and attain possible variations

--- spellcheck/spchk.c
+++ spellcheck/spchk.c
@@ -52,8 +52,130 @@ int create_dict(int fd) {
     return 0;
 }
 
+char* clean_text(char* word) {
+    // text -> {dict varitions}
+    // "apple" -> {"apple"}
+    // "Apple" -> {"apple", "Apple"}
+    // "APPLE" -> {"apple", "Apple", "APPLE"}
+    // "MacDonald" -> {"MacDonald"}
+    // "HeLlO" -> {"HeLlO"}
+
+    const int exactMatch = 1; // If none of these below are true, then the word must match exactly. Always true by default
+    int allLowercase = 0; // strictest case
+    int properCase = 0; // allLowercase will always be true if this is true
+    int allUppercase = 0; // allLowercase & properCase will always be true if this is true
+
+    int trailingPunctuation;
+    for (trailingPunctuation = strlen(word) - 1; trailingPunctuation >= 0; trailingPunctuation--) {
+        if ((word[trailingPunctuation] >= 'A' && word[trailingPunctuation] <= 'Z') || (word[trailingPunctuation] >= 'a' && word[trailingPunctuation] <= 'z')) {
+            break;
+        }
+    }
+
+    int leadingPunctuation;
+    for (leadingPunctuation = 0; leadingPunctuation < strlen(word); leadingPunctuation++) {
+        if ((word[leadingPunctuation] >= 'A' && word[leadingPunctuation] <= 'Z') || (word[leadingPunctuation] >= 'a' && word[leadingPunctuation] <= 'z')) {
+            break;
+        }
+    }
+
+    int numUppercase = 0;
+    int wordLen = trailingPunctuation - leadingPunctuation + 1;
+    if (wordLen <= 0) {
+        return "";
+    }
+
+    int ignorePunctuation = wordLen;
+    for (int ii = leadingPunctuation; ii <= trailingPunctuation; ii++) {
+        if (word[ii] >= 'A' && word[ii] <= 'Z') {
+            numUppercase += 1;
+            if (ii == 0) {
+                properCase = 1;
+            } else {
+                properCase = 0;
+            }
+        } else if (!(word[ii] >= 'a' && word[ii] <= 'z')) {
+            ignorePunctuation -= 1;
+        }
+    }
+    
+
+    if (numUppercase == ignorePunctuation) {
+        allLowercase = 1;
+        properCase = 1;
+        allUppercase = 1;
+    }
+    if (numUppercase == 0) {
+        allLowercase = 1;
+    }
+
+    char* parsed = word + leadingPunctuation;
+    parsed[wordLen] = '\0';
+    wordLen++;
+
+    int numVariations = exactMatch + allLowercase + properCase + allUppercase;
+
+    char* res = malloc(numVariations * (wordLen * sizeof(char))); // TODO: free this
+    if (res == NULL) {
+        perror("Error allocating memory");
+        return NULL;
+    }
+    
+    char* pointer = res;
+    if (exactMatch) {
+        pointer = strncpy(pointer, parsed, wordLen);
+        pointer = res + wordLen;
+    } else if (allLowercase) {
+        for (int ii = 0; ii < wordLen; ii++) {
+            if (parsed[ii] >= 'A' && parsed[ii] <= 'Z') {
+                parsed[ii] = parsed[ii] + 32;
+            }
+        }
+        pointer = strncpy(pointer, parsed, wordLen);
+        pointer = res + wordLen;
+    } else if (properCase) {
+        parsed[0] = parsed[0] - 32;
+        pointer = strncpy(pointer, parsed, wordLen);
+        pointer = res + wordLen;
+    } else if (allUppercase) {
+        for (int ii = 0; ii < wordLen; ii++) {
+            if (parsed[ii] >= 'a' && parsed[ii] <= 'z') {
+                parsed[ii] = parsed[ii] - 32;
+            }
+        }
+        pointer = strncpy(pointer, parsed, wordLen);
+    }
+    
+    return res;
+}
+
 int check_text(int fd) {
-    // read from file and check against trie
+    // read from file and check against trie1
+
+    ssize_t bytesRead = 0;
+    char buffer[BUFFER_SIZE];
+    if (buffer == NULL) {
+        perror("Error allocating memory");
+        return -1;
+    }
+    
+    int line_number = 0, col_number = 0;
+
+    // iterate through the text file
+
+    while ((bytesRead = read(fd, buffer, BUFFER_SIZE)) > 0) {
+        
+        // get the word in the line
+
+        // use a helper method to get a list the different possible variations of the word, if any
+
+        // check if any of the allowed variations (any word in the list) are in the dict
+
+        // if its not return error msg with line and col number
+
+
+    }
+
     return 0;
 }
 
@@ -77,7 +199,7 @@ int file_handler(const char* pathname, int (*func)()) {
 
 int main(int argc, char* argv[]) {
     if (argc <= 2) {
-        printf("Usage: ./spchk <dictionary file> <input file>\n");
+        fprintf(stderr, "Usage: ./spchk <dictionary file> <input file>\n");
         return EXIT_FAILURE;
     }
     
